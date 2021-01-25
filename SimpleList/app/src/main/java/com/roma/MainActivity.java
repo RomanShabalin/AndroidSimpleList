027355003f706796ead6aac2a4c9db10f6713c58
RomanShabalin <shabalin.roma@mail.ru>
2021-01-25 16:06:43 +0300

    first commit

---
+++ SimpleList/app/src/main/java/com/roma/MainActivity.java
@@ -0,0 +1,28 @@
+package com.roma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView plList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Matlab", "C", "C++", "Swift", "Objective C",
+        "Haskell", "Lisp", "Rust", "Pascal"};
+
+        plList.setText("");
+
+        for (String name : names) {
+            plList.append(name + "\n");
+        }
+    }
+}
